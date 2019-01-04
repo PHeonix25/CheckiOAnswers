8d0e7a1969ec03fdbefd4c5eefa9bf13001406fc
Pat Hermens <p@hermens.com.au>
2019-01-04 13:11:49 +0100

    Updated main function to actually 'do something'

--- CheckIOAnswers.py
+++ CheckIOAnswers.py
@@ -1,18 +1,33 @@
 import click
+import os
 
 @click.group()
-def checkioanswers():
+@click.option('-v', '--verbose', is_flag=True, default=False, help='Turn verbosity up (useful while debugging)')
+def checkioanswers(verbose):
+    if (verbose):
+        click.echo(click.style('Verbose mode is on', fg='yellow', bold=True))
     pass
 
 @checkioanswers.command()
-def cmd1():
-    '''Command on checkioanswers'''
-    click.echo('checkioanswers cmd1')
+def list():
+    '''List all the answers that I've produced'''
+    click.echo('Requested to list all answers')
+    for root, dirs, files in os.walk('.'):
+        for filename in files:
+            if (filename.endswith(".py") and filename[0].isdigit()):
+                click.echo("\t" + filename)
 
 @checkioanswers.command()
-def cmd2():
-    '''Command on checkioanswers'''
-    click.echo('checkioanswers cmd2')
+@click.argument('filename')
+def run(filename: str):
+    '''Given a filename, run the answer.'''
+    if (not os.path.isfile(filename)):
+        click.echo(click.style('Input provided was not a file', fg='red',bold=True), err=True)
+        return
+
+    click.echo('Requested to run \'%s\':' % filename)
+    click.echo('')
+    exec(open(filename).read(), globals())        
 
 if __name__ == '__main__':
     checkioanswers()
