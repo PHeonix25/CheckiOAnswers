2ce18a57ee1e0323c025a0870331f78edb075346
Pat Hermens <p@hermens.com.au>
2019-01-04 10:35:45 +0100

    Adding setup.py for Azure Pipelines

---
+++ setup.py
@@ -0,0 +1,20 @@
+﻿from distutils.core import setup
+import distutils
+
+setup(
+    name='CheckiOAnswers',
+    version='0.1.0',
+    author='Pat Hermens',
+    author_email='p@hermens.com.au',
+    packages=['checkioanswers', 'checkioanswers.test'],
+    scripts=['answers/*'],
+    url='https://github.com/PHeonix25/CheckiOAnswers',
+    license='LICENSE.txt',
+    description='My answers to the CheckiO challenges at https://py.checkio.org/.',
+    long_description=open('README.txt').read(),
+    install_requires=[
+        "click==6.7",
+        "py==1.4.34",
+        "pytest==3.2.3"
+    ],
+)
